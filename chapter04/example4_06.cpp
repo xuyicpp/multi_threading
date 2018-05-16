2590e882c0d5c9a24028afe98fdc230f2fd1c71b
小徐 <yixu_cs@hust.edu.cn>
2018-05-16 21:52:37 +0800

    add ch4

---
+++ chapter04/example4_06.cpp
@@ -0,0 +1,12 @@
+//使用std::future获取异步任务的返回值
+#include <future>
+#include <iostream>
+
+int find_the_answer_to_ltuae();
+void do_other_stuff();
+int main()
+{
+	std::future<int> the_anwer=std::async(find_the_answer_to_ltuae);
+	do_other_stuff();
+	std::cout<<"The answer is "<<the_anwer.get()<<std::endl;
+}
