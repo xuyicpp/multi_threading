7301459c2cb5fd438c16a389ab3d7f470a0aa595
小徐 <yixu_cs@hust.edu.cn>
2018-05-17 17:24:37 +0800

    add ch4

--- chapter04/example4_13.cpp
+++ chapter04/example4_13.cpp
@@ -18,7 +18,7 @@ std::list<T> parallel_quick_sort(std::list<T> input)
 	std::future<std::list<T> > new_lower(std::async(&parallel_quick_sort<T>,std::move(lower_part)));
 
 	auto new_higher(parallel_quick_sort(std::move(input)));
-
+	//splice剪切加粘贴
 	result.splice(result.end(),new_higher);
 	result.splice(result.begin(),new_lower.get());
 	return result;
